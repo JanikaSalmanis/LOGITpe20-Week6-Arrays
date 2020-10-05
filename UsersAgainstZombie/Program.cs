92622ecbbb3af603b22ca4b1d97d7dd9e1871fba
Opilane TTHK <opilane@tthk.ee>
2020-10-05 14:49:32 +0300

    first commit

---
+++ UsersAgainstZombie/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace UsersAgainstZombie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*The programm asks the user to enter the names of
+             five intems from the users bag.
+            The program displays a random pick from the users bag
+            to fight off zombies with.
+            "In case of zombie apocalypse you will have to
+            fight zombies with{random intem from the bag}"
+            */
+            string[] userBag = new string[5];
+            int i = 0;
+            while(i < userBag.Length)
+            {
+                Console.WriteLine("whats there in you bag?");
+                userBag[i] = Console.ReadLine();
+                i++;
+            }
+            Random rnd = new Random();
+            Console.WriteLine($"In case of zombie apocalypse you will have to +" +
+                $"fight zombies with{userBag[rnd.Next(0, userBag.Length)]}");
+        }
+    }
+}
---
+++ WardrobePicker/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Drawing;
+
+namespace WardrobePicker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] wardrobe = { "hat", "hoodie", "jeans", "dress", "shirt" };
+            string[] color = { "red", "blue", "white", "green", "pink" };
+            string[] patterns = { "flowery", "striped", "polka dot", "circles", "checkered" };
+            /*The program asks if the user needs help with picking an outfift
+             * If the users answer no, the console dispays "Farewell"
+             * If users confirms, the program displays the combination of pattern, color and cholting intem "Today you should wear..*/
+            Random rnd = new Random();
+            Console.WriteLine("Would you like some fashio advice?");
+            string userInput = Console.ReadLine();
+            if(userInput == "no")
+            {
+                Console.WriteLine("Farewell!");
+            }else
+            {
+                Console.WriteLine($"Today you should wear " +
+                    $"{color[rnd.Next(0, color.Length)]} " +
+                    $"{patterns[rnd.Next(0, patterns.Length)]} " +
+                    $"{wardrobe[rnd.Next(0, wardrobe.Length)]}");
+            }
+
+        }
+    }
+}
